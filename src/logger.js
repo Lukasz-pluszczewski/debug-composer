2ee3e48edcf9a773e7f5346911b9963fe73ae093
lukasz_brainhub <lukasz@brainhub.pl>
2017-03-24 16:43:20 +0100

    Added performance tools to logger

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,66 @@
 import debug from 'debug';
 
+const createTooler = (namespace, debugInstances) => {
+  const timestamps = {};
+  return {
+    time(name, log = false) {
+      timestamps[name] = performance.now();
+      if (log) {
+        if (!debugInstances[`${namespace}:time`]) {
+          debugInstances[`${namespace}:time`] = debug(`${namespace}:time`);
+        }
+        debugInstances[`${namespace}:time`](`${name}: ${timestamps[name]}ms`);
+      }
+      return performance.now();
+    },
+    timeEnd(name, log = true) {
+      let time = performance.now();
+      if (timestamps[name]) {
+        time = time - timestamps[name];
+      }
+      if (log) {
+        if (!debugInstances[`${namespace}:time`]) {
+          debugInstances[`${namespace}:time`] = debug(`${namespace}:time`);
+        }
+        debugInstances[`${namespace}:time`](`${name}: ${time}ms`);
+      }
+      return time;
+    },
+    testPerformance(cb, times = 1, log = true) {
+      let time = 0;
+      if (times === 1) {
+        time = performance.now();
+        cb();
+        time = performance.now() - time;
+      } else {
+        time = performance.now();
+        for (let i = 0; i <= times; i++) {
+          cb();
+        }
+        time = performance.now() - time;
+      }
+      if (log) {
+        if (!debugInstances[`${namespace}:performance`]) {
+          debugInstances[`${namespace}:performance`] = debug(`${namespace}:performance`);
+        }
+        debugInstances[`${namespace}:performance`](
+          `Function triggered ${times === 1 ? 'once' : `${times} times`}. Measured time: ${time}ms`
+        );
+      }
+
+      return time;
+    }
+  };
+};
+
 const createLogger = (namespace, addToGlobals = false) => {
   const debugInstances = {};
+  const tooler = createTooler(namespace, debugInstances);
   const logger = new Proxy({}, {
     get(target, name) {
+      if (tooler[name]) {
+        return tooler[name];
+      }
       if (!debugInstances[`${namespace}:${name}`]) {
         debugInstances[`${namespace}:${name}`] = debug(`${namespace}:${name}`);
       }

0d888f5839a2bacaf8c597f0a5ae1aefccf3fb2c
lukasz_brainhub <lukasz@brainhub.pl>
2017-03-31 16:24:51 +0200

    Updated exports

---
+++ docs/whatsnew.md
@@ -0,0 +1,3 @@
+#### 0.1.2
+ * added performance tools to logger (time, timeEnd and testPerformance methods)
+ * added Debug component to show some debug data in a movable window (WORK IN PROGRESS)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import debugTools from './debugTools';
+import debugToolsImp from './debugTools';
 import createLogger from './logger';
 
+export const debugTools = debugToolsImp;
 export const configureDebugger = debugTools.configureDebugger;
 export const resetDebugger = debugTools.clearLocalStorage;
 export default createLogger;
